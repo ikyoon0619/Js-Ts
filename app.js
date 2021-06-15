0d9f665660cd24bf7b54c5273049499ac302a191
ikyoon0619 <ikyoon0619@gmail.com>
2021-06-15 23:56:00 +0900

    index.html: tailwind css 추가, app.js: template 방식으로 교체

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const container = document.getElementById('root')
 const ajax = new XMLHttpRequest();
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
-const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+const store = {
+  currentPage: 1,
+}
 
 function getData(url) {
   ajax.open('GET', url, false);
@@ -16,33 +19,46 @@ function newsFeed() {
   const newsFeed = getData(NEWS_URL)
   const newsList = [];
 
-  newsList.push('<ul>');
+  let template = `
+    <div class="container mx-auto p-4">
+      <h1>Hacker News</h1>
+      <ul>
+        {{__news_feed__}}      
+      </ul>
+      <div>
+        <a href="#/page/{{__prev_page__}}">이전 페이지</a>
+        <a href="#/page/{{__next_page__}}">다음 페이지</a>
+      </div>
+    </div>
+  `;
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
     newsList.push(`
     <li>
-      <a href='#${newsFeed[i].id}'>
+      <a href='#/show/${newsFeed[i].id}'>
       ${newsFeed[i].title} (${newsFeed[i].comments_count})
       </a>
     </li>
   `);
   }
-  newsList.push('</ul>')
 
-  container.innerHTML = newsList.join('');
+  template = template.replace('{{__news_feed__}}', newsList.join(''));
+  template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
+  template = template.replace('{{__next_page__}}', store.currentPage + 1);
+
+  container.innerHTML = template;
 
 }
 
 // 글 내용 보여주기
 function newsDetail() {
-  const id = location.hash.substr(1)
-
+  const id = location.hash.substr(7)
   const newsContent = getData(CONTENT_URL.replace('@id', id))
 
   container.innerHTML = `
       <h1>${newsContent.title}</h1>
       <div>
-        <a href="#">목록으로</a>
+        <a href="#/page/${store.currentPage}">목록으로</a>
       </div>
     `;
 }
@@ -51,6 +67,9 @@ function router() {
   const routePath = location.hash;
 
   if (routePath === "") {
+    newsFeed();
+  } else if (routePath.indexOf('#/page/') >= 0) {
+    store.currentPage = Number(routePath.substr(7))
     newsFeed();
   } else {
     newsDetail();
