be30d2e420fd717598454fc34b5cb006ff30523a
allegory_one <ikyoon0619@gmail.com>
2021-06-15 17:09:57 +0900

    app.js: 페이징 구현

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const container = document.getElementById('root')
 const ajax = new XMLHttpRequest();
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'
+const store ={
+  currentPage: 1,
+}
 
 function getData(url) {
   ajax.open('GET', url, false);
@@ -15,34 +18,46 @@ function getData(url) {
 function newsFeed() {
   const newsFeed = getData(NEWS_URL)
   const newsList = [];
+  let template = `
+  <div class="container mx-auto p-4">
+    <h1>Hacker News</h1>
+    <ul>
+      {{__news_feed__}}
+    </ul>
+    <div>
+     <a href="#/page/{{__prev_page__}}">이전 페이지</a>
+     <a href="#/page/{{__next_page__}}">다음 페이지</a>
+    </div>
+  </div>
+  `;
 
-  newsList.push('<ul>');
-
-  for (let i = 0; i < 10; i++) {
+  for (let i = (store.currentPage - 1) * 10 ; i < store.currentPage * 10; i++) {
     newsList.push(`
     <li>
-      <a href='#${newsFeed[i].id}'>
+      <a href='#/show/${newsFeed[i].id}'>
       ${newsFeed[i].title} (${newsFeed[i].comments_count})
       </a>
     </li>
   `);
   }
-  newsList.push('</ul>')
-
-  container.innerHTML = newsList.join('');
-
+  
+  template = template.replace('{{__news_feed__}}', newsList.join(''))
+  template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1)
+  template = template.replace('{{__next_page__}}',store.currentPage*10 < newsFeed.length ? store.currentPage + 1 : store.currentPage )
+  
+  container.innerHTML = template;
 }
 
 // 글 내용 보여주기
 function newsDetail() {
-  const id = location.hash.substr(1)
+  const id = location.hash.substr(7)
 
   const newsContent = getData(CONTENT_URL.replace('@id', id))
 
   container.innerHTML = `
       <h1>${newsContent.title}</h1>
       <div>
-        <a href="#">목록으로</a>
+        <a href="#/page/${store.currentPage}">목록으로</a>
       </div>
     `;
 }
@@ -52,7 +67,11 @@ function router() {
 
   if (routePath === "") {
     newsFeed();
-  } else {
+  } else if(routePath.indexOf('#/page/') >= 0){
+  
+    store.currentPage = Number(routePath.substr(7));
+    newsFeed();
+  } else{
     newsDetail();
   }
 }
